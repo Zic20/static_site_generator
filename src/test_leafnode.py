07f8da02ca34f2ed6b06117f9d34552577e5dcfe
zico <izallyjr@gmail.com>
2025-05-21 01:14:20 -0400

    Created the LeafNode class and wrote tests for the class

---
+++ src/test_leafnode.py
@@ -0,0 +1,16 @@
+import unittest
+
+from leafnode import LeafNode
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+    
+    def test_to_html_returns_raw_text(self):
+        node = LeafNode(None,"Hello, world!")
+        self.assertEqual(node.to_html(),"Hello, world!")
+    
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), '<a href="https://www.google.com">Click me!</a>')
