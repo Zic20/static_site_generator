8f47aa14a4c58fc036180575d19d0ff49d722037
zico <izallyjr@gmail.com>
2025-05-28 22:34:08 -0400

    added two functions for extracting markdown_images and markdown_links and tests for each function

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from textnode import TextNode, TextType
 from htmlnode import HTMLNode
 from leafnode import LeafNode
+import re
 
 def main():
     text_node = TextNode("This is some anchor text",TextType.LINK,"https://www.boot.dev")
@@ -27,24 +28,4 @@ def text_node_to_html_node(text_node):
         case _:
             raise Exception("invalid text type given to text_node")
         
-def split_nodes_delimiter(old_nodes, delimiter, text_type):
-    new_nodes = []
-    for old_node in old_nodes:
-        if old_node.text_type != TextType.TEXT:
-            new_nodes.append(old_node)
-            continue
-        split_nodes = []
-        sections = old_node.text.split(delimiter)
-        if len(sections) % 2 == 0:
-            raise ValueError("invalid markdown, formatted section not closed")
-        for i in range(len(sections)):
-            if sections[i] == "":
-                continue
-            if i % 2 == 0:
-                split_nodes.append(TextNode(sections[i], TextType.TEXT))
-            else:
-                split_nodes.append(TextNode(sections[i], text_type))
-        new_nodes.extend(split_nodes)
-    return new_nodes
-    
 main()
