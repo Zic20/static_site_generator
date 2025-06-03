cfff6996c3ae5aeb33927cbe37edd4bc2c313490
zico <izallyjr@gmail.com>
2025-06-02 23:59:32 -0400

    completed the block_markdown class and its tests

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,5 +1,9 @@
 from enum import Enum
 
+from inline_markdown import text_node_to_html_node, text_to_textnodes
+from parentnode import ParentNode
+from textnode import TextNode, TextType
+
 class BlockType(Enum):
     PARAGRAPH = "paragraph"
     HEADING = "heading"
@@ -72,3 +76,93 @@ def is_closing_char_valid(char,text,target_count,minimunCount = 1):
     if count > target_count or count < minimunCount:
         return count,False
     return count, True
+
+def text_to_children(text):
+    text_nodes = text_to_textnodes(text)
+    children = []
+    for text_node in text_nodes:
+        html_node = text_node_to_html_node(text_node)
+        children.append(html_node)
+    return children
+
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+    children = []
+    for block in blocks:
+        children.append("hello")
+    return ParentNode("div",children)
+
+
+def block_to_html_node(block):
+    block_type = block_to_block_type(block)
+    
+    match(block_type):
+        case(BlockType.PARAGRAPH):
+            return paragraph_to_html_node(block)
+        case(BlockType.HEADING):
+            return heading_to_html_node(block)
+        case(BlockType.CODE):
+            return code_to_html_node(block)
+        case(BlockType.UNORDERED_LIST):
+            return ordered_list_to_html_node(block)
+        case(BlockType.ORDERED_LIST):
+            return ordered_list_to_html_node(block)
+        case(BlockType.QUOTE):
+            return quote_to_html_node(block)
+        case _:
+            raise ValueError("invalid block type")
+
+
+def paragraph_to_html_node(block):
+    lines = block.split("\n")
+    paragraph = " ".join(lines)
+    children = text_to_children(paragraph)
+    return ParentNode("p",children)
+
+def heading_to_html_node(block):
+    count,is_heading_valid = is_leading_char_valid("#","block",6,1)
+    
+    if is_heading_valid == False:
+        raise ValueError(f"invalid heading level: {count}")
+    
+    text = block[count + 1:]
+    children = text_to_children(text)
+    return children
+
+def code_to_html_node(block):
+    if not block.startswith("```") or not block.endswith("```"):
+        raise ValueError('invalid code blockl')
+    text = block[4:-3]
+    raw_text_node = TextNode(text, TextType.Text)
+    child = text_node_to_html_node(raw_text_node)
+    code = ParentNode("code",[child])
+    return ParentNode("pre",[code])
+
+def ordered_list_to_html_node(block):
+    items = block.split("\n")
+    html_items = []
+    for item in items:
+        text = item[2:]
+        children = text_to_children(text)
+        html_items.append(ParentNode("li",children))
+    return ParentNode("ol",html_items)
+
+def unordered_list_to_html_list(block):
+    items = block.split("\n")
+    html_items = []
+    for item in items:
+        text = item[2:]
+        children = text_to_children(text)
+        html_items.append(ParentNode("li",children))
+    return ParentNode("ul",html_items)
+
+def quote_to_html_node(block):
+    lines = block.split("\n")
+    new_lines = []
+    for line in lines:
+        if not line.startswith(">"):
+            raise ValueError("invalid quote block")
+        new_lines.append(line.lstrip(">").strip())
+    content = " ".join(new_lines)
+    children = text_to_children(content)
+    return ParentNode("blockquote", children)
